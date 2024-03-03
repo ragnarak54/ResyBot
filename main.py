8f29eb391c22515783bc799c7141bc6e62dda4b1
ragnarak54 <colpolmen@gmail.com>
2024-03-03 16:06:40 -0700

    cleanup

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(intents=intents, command_prefix=['?', '!'])
-bot.pending_snipes = {}
 
 
 @bot.event
